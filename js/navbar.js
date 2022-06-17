ea3ef4100005260a1dff6226ac0c8d9a6840cbc7
CST1229 <68464103+CST1229@users.noreply.github.com>
2022-06-17 16:38:57 +0200

    fix logoout and username not appearing on navbar

--- js/navbar.js
+++ js/navbar.js
@@ -25,13 +25,14 @@
 	
 	document.head.after(navbar); 
 	
+	let auth;
 	try {
-	const auth = await window.getAuth();
+		auth = await window.getAuth();
 	} catch(e) {return};
 	
 	if (auth.error) return;
-	
 	document.getElementById("login-user").textContent = auth.username;
+	
 	window.logOut = async function() {
 		await fetch("https://appelldb-server.cst1229.repl.co/logout?sessionId=" + auth.sessionId);
 		location.reload();
